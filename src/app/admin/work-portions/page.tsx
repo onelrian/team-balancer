a9ca52c6f38f302327de595cd8eabd6f3347d44e
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:21:21 +0100

    Add admin work portions management page

---
+++ src/app/admin/work-portions/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function WorkPortionsManagement() {
+  return (
+    <div>
+      <h1>Work Portions Management</h1>
+      <p>This page will allow you to manage work portions and their weights.</p>
+    </div>
+  );
+}
