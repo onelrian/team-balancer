70dae2db09109a1b9b94c48686b3ae41eccca5dc
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:20:35 +0100

    Add admin user classes management page

---
+++ src/app/admin/user-classes/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function UserClassesManagement() {
+  return (
+    <div>
+      <h1>User Classes Management</h1>
+      <p>This page will allow you to manage user classes and assign users to classes.</p>
+    </div>
+  );
+}
