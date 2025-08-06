caca6fb51dfe5f69d09558e165df7ba7a425baad
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:20:48 +0100

    Add admin users management page

---
+++ src/app/admin/users/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function UserManagement() {
+  return (
+    <div>
+      <h1>User Management</h1>
+      <p>This page will allow you to manage users and their roles.</p>
+    </div>
+  );
+}
