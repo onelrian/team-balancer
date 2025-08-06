d3b68bfe0d765601f803286574d993a9f172b176
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:24:44 +0100

    Add user preferences page

---
+++ src/app/dashboard/preferences/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function WorkloadPreferences() {
+  return (
+    <div>
+      <h1>Workload Preferences</h1>
+      <p>This page will allow you to set your preferences for different work portions.</p>
+    </div>
+  );
+}
