2e07f9dedf7b839dcb238e9f36b9fa2f0b285bf9
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:20:24 +0100

    Add admin assignments management page

---
+++ src/app/admin/assignments/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function AssignmentsManagement() {
+  return (
+    <div>
+      <h1>Assignments Management</h1>
+      <p>This page will allow you to generate new assignments and view assignment history.</p>
+    </div>
+  );
+}
