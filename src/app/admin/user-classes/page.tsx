1e972e6618de0350516aef5972f2bd23f0ddcecc
dericko681 <abonghoderick@gmail.com>
2025-08-06 09:54:46 +0100

    feat: Update admin user classes page

--- src/app/admin/user-classes/page.tsx
+++ src/app/admin/user-classes/page.tsx
@@ -4,7 +4,13 @@ export default function UserClassesManagement() {
   return (
     <div>
       <h1>User Classes Management</h1>
-      <p>This page will allow you to manage user classes and assign users to classes.</p>
+      <p>This page allows you to manage user classes and assign users to classes.</p>
+      <ul>
+        <li>Create and edit user classes</li>
+        <li>Assign users to classes</li>
+        <li>Remove users from classes</li>
+        <li>View class members</li>
+      </ul>
     </div>
   );
 }
